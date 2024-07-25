ec5b352a3d3841f2b83898d15f61744feca9f203
Manaswita10 <131282450+Manaswita10@users.noreply.github.com>
2024-07-26 03:04:28 +0530

    Update settings.py

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '@k0#p3kidu)yaaa3u1hplxz)f@^6xiy384*(+n@@s5x#1bx@m5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app, 'now.sh']
 
 
 # Application definition
@@ -126,9 +126,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
-STATIC_DIR,
- ]
+STATICFILES_DIRS= os.path.join(BASE_DIR,'static'),
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build,'static')
+ 
 
 LOGIN_REDIRECT_URL='/afterlogin'
 
